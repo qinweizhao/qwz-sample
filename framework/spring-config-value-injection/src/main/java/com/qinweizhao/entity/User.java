3f677c64f2ae7b86f436687c7eac3b16e46253ab
YVKG <yvkg@qq.com>
2021-11-20 23:54:10 +0800

    commit

--- framework/spring-config-value-injection/src/main/java/com/qinweizhao/entity/User.java
+++ framework/spring-config-value-injection/src/main/java/com/qinweizhao/entity/User.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.validation.constraints.Email;
-
 /**
  * * <bean class="User">
  * *      <property name="name" value="字面量/${key}从环境变量、配置文件中获取值/#{SpEL}"></property>
