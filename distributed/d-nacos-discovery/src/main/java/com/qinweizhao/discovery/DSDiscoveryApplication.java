a27a68f887466df51558d3b60263005f525273aa
YVKG <yvkg@qq.com>
2022-03-01 19:27:33 +0800

    commit

--- distributed/d-nacos-discovery/src/main/java/com/qinweizhao/discovery/DSDiscoveryApplication.java
+++ distributed/d-nacos-discovery/src/main/java/com/qinweizhao/discovery/DSDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.qinweizhao.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class DsDiscoveryApplication {
+public class DSDiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DsDiscoveryApplication.class, args);
+        SpringApplication.run(DSDiscoveryApplication.class, args);
     }
 
 }
--- distributed/d-nacos-discovery/src/test/java/com/qinweizhao/discovery/DsDSDiscoveryApplicationTests.java
+++ distributed/d-nacos-discovery/src/test/java/com/qinweizhao/discovery/DsDSDiscoveryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DsDiscoveryApplicationTests {
+class DsDSDiscoveryApplicationTests {
 
     @Test
     void contextLoads() {
