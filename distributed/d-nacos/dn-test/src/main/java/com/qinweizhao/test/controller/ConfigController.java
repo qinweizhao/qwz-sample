1028c327c43bace517952a24dd9072f35a4ec1db
YVKG <yvkg@qq.com>
2022-03-18 00:15:58 +0800

    commit

---
+++ distributed/d-nacos/dn-test/src/main/java/com/qinweizhao/test/controller/ConfigController.java
@@ -0,0 +1,26 @@
+package com.qinweizhao.test.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author qinweizhao
+ */
+@RestController
+//动态刷新
+@RefreshScope
+public class ConfigController {
+
+    @Value("${test.user.name}")
+    private String name;
+
+    @Value("${test.user.age}")
+    private String age;
+
+    @GetMapping("info")
+    public String get() {
+        return name + age;
+    }
+}
