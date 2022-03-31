3ac9b3c858cd0c55bf2b1db945a0ed9e1cd3726b
YVKG <yvkg@qq.com>
2022-03-31 19:53:14 +0800

    commit

---
+++ basic/b-jvm/src/main/java/com/qinweizhao/jvm/runtimedataareas/PcRegister.java
@@ -0,0 +1,16 @@
+package com.qinweizhao.jvm.runtimedataareas;
+
+/**
+ * @author qinweizhao
+ * @since 2022/3/31
+ */
+public class PcRegister {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+        String s = "qwz";
+        System.out.println(i);
+        System.out.println(k);
+    }
+}
