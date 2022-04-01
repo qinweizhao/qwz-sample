494cc4a596df64ecd326f7edaea39a967490fe92
YVKG <yvkg@qq.com>
2022-04-01 20:11:57 +0800

    commit

---
+++ basic/b-jvm/src/main/java/com/qinweizhao/jvm/jni/IHaveNatives.java
@@ -0,0 +1,26 @@
+package com.qinweizhao.jvm.jni;
+
+/**
+ * 本地方法
+ * 标识符 native 可以与其他所有的 Java 标识符连用，但是 abstract 除外。
+ * @author qinweizhao
+ * @since 2022/4/1
+ */
+public  class IHaveNatives {
+
+    //abstract 没有方法体
+    // public abstract void abstractMethod(int x);
+
+    //native 和 abstract不能共存，native是有方法体的，由C语言来实现
+    public native void Native1(int x);
+
+    /**
+     使用native标识为本地方法接口
+     */
+    native static public long Native2();
+
+    native synchronized private float Native3(Object o);
+
+    native void Native4(int[] array) throws Exception;
+
+}
