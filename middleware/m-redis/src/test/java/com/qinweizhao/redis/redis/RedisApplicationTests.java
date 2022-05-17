cb73addd22818f64c860593081416c10e6dd3c39
YVKG <yvkg@qq.com>
2022-05-18 00:27:35 +0800

    commit

---
+++ middleware/m-redis/src/test/java/com/qinweizhao/redis/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qinweizhao.redis.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
