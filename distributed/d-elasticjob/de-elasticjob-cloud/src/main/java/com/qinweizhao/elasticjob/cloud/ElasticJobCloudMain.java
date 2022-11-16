c609cfdcd252f658e8c54541319cbaf5c225954a
YVKG <yvkg@qq.com>
2022-11-16 10:06:46 +0800

    commit

--- distributed/d-elasticjob/de-elasticjob-cloud/src/main/java/com/qinweizhao/elasticjob/cloud/ElasticJobCloudMain.java
+++ distributed/d-elasticjob/de-elasticjob-cloud/src/main/java/com/qinweizhao/elasticjob/cloud/ElasticJobCloudMain.java
@@ -2,12 +2,21 @@ package com.qinweizhao.elasticjob.cloud;
 
 
 import com.qinweizhao.elasticjob.cloud.job.MyJob;
-import org.apache.shardingsphere.elasticjob.cloud.api.JobBootstrap;
+import org.apache.shardingsphere.elasticjob.api.JobConfiguration;
+import org.apache.shardingsphere.elasticjob.cloud.executor.local.LocalTaskExecutor;
 
 public class ElasticJobCloudMain {
 
     public static void main(String[] args) {
-        JobBootstrap.execute(new MyJob());
+        // 创建作业配置
+        JobConfiguration jobConfig = JobConfiguration.newBuilder("myJob", 3).cron("0/5 * * * * ?").build();
+
+        // 配置当前运行的作业的分片项
+        int shardingItem = 1;
+
+        // 创建本地执行器
+        new LocalTaskExecutor(new MyJob(), jobConfig, shardingItem).execute();
+
     }
 
 }
