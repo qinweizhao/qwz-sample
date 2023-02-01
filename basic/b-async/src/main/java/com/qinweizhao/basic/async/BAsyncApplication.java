2295a1c5b3366e4b5cf0078931547d93d54df86f
YVKG <yvkg@qq.com>
2023-02-01 14:04:17 +0800

    commit

---
+++ basic/b-async/src/main/java/com/qinweizhao/basic/async/BAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.qinweizhao.b.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BAsyncApplication.class, args);
+	}
+
+}
