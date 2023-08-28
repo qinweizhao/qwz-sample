51a66af41df449cc704fad4c440a94d42ee78730
YVKG <yvkg@qq.com>
2023-08-28 10:59:07 +0800

    commit

--- basic/b-pdf/src/main/java/com/qinweizhao/basic/pdf/canvas/PatternCanvas.java
+++ basic/b-pdf/src/main/java/com/qinweizhao/basic/pdf/canvas/PatternCanvas.java
@@ -17,7 +17,7 @@ import com.lowagie.text.pdf.PdfPatternPainter;
 public class PatternCanvas {
 	/**
 	 * 网格
-	 * 
+	 *
 	 * @param pcb
 	 * @return: PdfPatternPainter
 	 * @since 1.0
@@ -28,7 +28,7 @@ public class PatternCanvas {
 
 //		float[] array1 =  {0.3F,0.3F,0.3F,0.3F,0.3F,0.3F};
 
-		painter.setLineDash(1,3,8F);
+		painter.setLineDash(1, 3, 8F);
 //		painter.setLineCap(LINE_CAP_PROJECTING_SQUARE);
 //		painter.setLineCap(LINE_CAP_PROJECTING_SQUARE);
 
@@ -79,6 +79,224 @@ public class PatternCanvas {
 		painter.lineTo(8, 4);
 		painter.stroke();
 
+		return painter;
+	}
+
+
+	/**
+	 * 点线 5%
+	 *
+	 * @param pcb pcb
+	 * @return PdfPatternPainter
+	 */
+	public static PdfPatternPainter dottedLine_5_1(PdfContentByte pcb) {
+		float base=1.55f;
+//		float base=1.65f;
+//		float base=3.11f;
+//		float base=2f;
+		PdfPatternPainter painter = pcb.createPattern(8/base, 8/base, null);
+		painter.setLineWidth(0.1f);
+		// 左上角
+		painter.rectangle(0, 0, 0.5f/base, 0.5f/base);
+		// 右上角
+		painter.rectangle(7.5f/base, 0, 0.5f/base, 0.5f/base);
+		// 左下角
+		painter.rectangle(0, 7.5f/base, 0.5f/base, 0.5f/base);
+		// 右下角
+		painter.rectangle(7.5f/base, 7.5f/base, 0.5f/base, 0.5f/base);
+		// 中心
+		painter.rectangle(3.5f/base, 3.5f/base, 1/base, 1/base);
+		painter.fill();
+		return painter;
+	}
+
+
+
+	public static PdfPatternPainter dottedLine_5_2(PdfContentByte pcb) {
+		float base=1.65f;
+//		float base=1.65f;
+//		float base=3.11f;
+//		float base=2f;
+//		PdfPatternPainter painter = pcb.createPattern(16/base, 8/base, null);
+		PdfPatternPainter painter = pcb.createPattern(16/base, 8/base, null);
+		painter.setLineWidth(0.1f);
+		// 左上角
+		painter.rectangle(5.5f/base, 1.5f/base, 1f/base, 1f/base);
+		// 右上角
+		painter.rectangle(13.5f/base, 1.5f/base, 1f/base, 1f/base);
+		// 左下角
+		painter.rectangle(1.5f/base, 5.5f/base, 1f/base, 1f/base);
+		// 右下角
+		painter.rectangle(9.5f/base, 5.5f/base, 1f/base, 1f/base);
+
+		painter.fill();
+		return painter;
+	}
+
+
+	public static PdfPatternPainter dottedLine_5(PdfContentByte pcb) {
+		float base=1.5f;
+//		float base=1.65f;
+//		float base=3.11f;
+//		float base=2f;
+//		PdfPatternPainter painter = pcb.createPattern(16/base, 8/base, null);
+		PdfPatternPainter painter = pcb.createPattern(8/base, 8/base, null);
+		painter.setLineWidth(0.1f);
+		// 左上角
+		painter.rectangle(1.5f/base, 1.5f/base, 1f/base, 1f/base);
+		// 右上角
+		painter.rectangle(5.5f/base, 5.5f/base, 1f/base, 1f/base);
+//		// 左下角
+//		painter.rectangle(1.5f/base, 5.5f/base, 1f/base, 1f/base);
+//		// 右下角
+//		painter.rectangle(9.5f/base, 5.5f/base, 1f/base, 1f/base);
+
+		painter.fill();
+		return painter;
+	}
+
+
+
+
+
+	public static PdfPatternPainter dottedLine_10(PdfContentByte pcb) {
+		float base=1.65f;
+//		float base=1.65f;
+//		float base=3.11f;
+//		float base=2f;
+		PdfPatternPainter painter = pcb.createPattern(8/base, 4/base, null);
+		painter.setLineWidth(0.1f);
+		painter.rectangle(0f/base, 3f/base, 1f/base, 1f/base);
+		painter.rectangle(4f/base, 1f/base, 1f/base, 1f/base);
+
+
+		painter.fill();
+		return painter;
+	}
+
+
+	/**
+	 * 浅色下对角线
+	 * @param pcb pcb
+	 * @return PdfPatternPainter
+	 */
+	public static PdfPatternPainter style_0(PdfContentByte pcb) {
+		float base=1.4f;
+//		float base=1.65f;
+//		float base=3.11f;
+//		float base=2f;
+		PdfPatternPainter painter = pcb.createPattern(4/base, 4/base, null);
+		//painter.setLineWidth(0.1f);
+		painter.rectangle(0f/base, 3f/base, 0.8f/base, 0.8f/base);
+		painter.rectangle(1f/base, 2f/base, 0.8f/base, 0.8f/base);
+		painter.rectangle(2f/base, 1/base, 0.8f/base, 0.8f/base);
+		painter.rectangle(3f/base, 0f/base, 0.8f/base, 0.8f/base);
+
+		painter.fill();
+		return painter;
+	}
+
+
+
+
+
+	public static PdfPatternPainter dottedLine_20(PdfContentByte pcb) {
+		float ratio=1.2f;
+//		float base=1.65f;
+//		float base=3.11f;
+//		float base=2f;
+		PdfPatternPainter painter = pcb.createPattern(4/ratio, 4/ratio, null);
+		painter.setLineWidth(0.1f);
+		painter.rectangle(0f/ratio, 3f/ratio, 0.8f/ratio, 0.8f/ratio);
+		painter.rectangle(2f/ratio, 1f/ratio, 0.8f/ratio, 0.8f/ratio);
+
+		painter.fill();
+		return painter;
+	}
+
+
+	public static PdfPatternPainter dottedLine_25(PdfContentByte pcb) {
+		float ratio=1.2f;
+//		float base=1.65f;
+//		float base=3.11f;
+//		float base=2f;
+		PdfPatternPainter painter = pcb.createPattern(4/ratio, 2/ratio, null);
+		painter.setLineWidth(0.1f);
+		painter.rectangle(0f/ratio, 1f/ratio, 0.8f/ratio, 0.8f/ratio);
+		painter.rectangle(2f/ratio, 0f/ratio, 0.8f/ratio, 0.8f/ratio);
+
+		painter.fill();
+		return painter;
+	}
+
+
+	public static PdfPatternPainter dottedLine_30(PdfContentByte pcb) {
+		float ratio=1.3f;
+
+		float width = 1f/ratio;
+		float height = 1f/ratio;
+
+		PdfPatternPainter painter = pcb.createPattern(4/ratio, 4/ratio, null);
+		painter.setLineWidth(0.1f);
+		painter.rectangle(0f/ratio, 3f/ratio, width, height);
+		painter.rectangle(1f/ratio, 0f/ratio, width, height);
+		painter.rectangle(1f/ratio, 2f/ratio, width, height);
+		painter.rectangle(2f/ratio, 1f/ratio, width, height);
+		painter.rectangle(3f/ratio, 0f/ratio, width, height);
+		painter.rectangle(3f/ratio, 2f/ratio, width, height);
+
+		painter.fill();
+		return painter;
+	}
+
+
+
+
+	public static PdfPatternPainter dottedLine_40(PdfContentByte pcb) {
+
+		float ratio=0.75F;
+//
+//		float width = 0.87f*ratio;
+//		float height = 0.87f*ratio;
+
+		float width = 1f*ratio;
+		float height = 1f*ratio;
+
+		PdfPatternPainter painter = pcb.createPattern(8*ratio, 8*ratio, null);
+//		painter.setLineWidth(ratio);
+		painter.rectangle(0f*ratio, 1f*ratio, width, height);
+		painter.rectangle(0f*ratio, 3f*ratio, width, height);
+		painter.rectangle(0f*ratio, 5f*ratio, width, height);
+		painter.rectangle(0f*ratio, 7f*ratio, width, height);
+		painter.rectangle(1f*ratio, 0f*ratio, width, height);
+		painter.rectangle(1f*ratio, 2f*ratio, width, height);
+		painter.rectangle(1f*ratio, 4f*ratio, width, height);
+		painter.rectangle(2f*ratio, 1f*ratio, width, height);
+		painter.rectangle(2f*ratio, 3f*ratio, width, height);
+		painter.rectangle(2f*ratio, 5f*ratio, width, height);
+		painter.rectangle(2f*ratio, 7f*ratio, width, height);
+		painter.rectangle(3f*ratio, 0f*ratio, width, height);
+		painter.rectangle(3f*ratio, 2f*ratio, width, height);
+		painter.rectangle(3f*ratio, 4f*ratio, width, height);
+		painter.rectangle(3f*ratio, 6f*ratio, width, height);
+		painter.rectangle(4f*ratio, 1f*ratio, width, height);
+		painter.rectangle(4f*ratio, 3f*ratio, width, height);
+		painter.rectangle(4f*ratio, 5f*ratio, width, height);
+		painter.rectangle(4f*ratio, 7f*ratio, width, height);
+		painter.rectangle(5f*ratio, 0f*ratio, width, height);
+		painter.rectangle(5f*ratio, 4f*ratio, width, height);
+		painter.rectangle(5f*ratio, 6f*ratio, width, height);
+		painter.rectangle(6f*ratio, 1f*ratio, width, height);
+		painter.rectangle(6f*ratio, 3f*ratio, width, height);
+		painter.rectangle(6f*ratio, 5f*ratio, width, height);
+		painter.rectangle(6f*ratio, 7f*ratio, width, height);
+		painter.rectangle(7f*ratio, 0f*ratio, width, height);
+		painter.rectangle(7f*ratio, 2f*ratio, width, height);
+		painter.rectangle(7f*ratio, 4f*ratio, width, height);
+		painter.rectangle(7f*ratio, 6f*ratio, width, height);
+
+
+		painter.fill();
 		return painter;
 	}
 }
--- basic/b-pdf/src/main/java/com/qinweizhao/basic/pdf/sample/DiamondGrid.java
+++ basic/b-pdf/src/main/java/com/qinweizhao/basic/pdf/sample/DiamondGrid.java
@@ -30,7 +30,7 @@ public class DiamondGrid {
             // 文档对象 实现A4纸页面
             Document document = new Document(PageSize.A4);
             // 设置文档的页边距就是距离页面边上的距离，分别为：左边距，右边距，上边距，下边距
-            document.setMargins(700, 700, 700, 700);
+            //document.setMargins(700, 700, 700, 700);
             // 这个是生成破pdf的位置以及名称
 
             PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(fileName));
@@ -39,25 +39,35 @@ public class DiamondGrid {
             // step 4: we grab the ContentByte and do some stuff with it
             PdfContentByte pcb = pdfWriter.getDirectContent();
             // an example of a rectangle with a diagonal in very thick lines
-            pcb.setLineWidth(0f);
+//            pcb.setLineWidth(0f);
 //            pcb.setColorStroke(Color.black);
 
 
-            PdfPatternPainter painter= PatternCanvas.grid(pcb);
-            pcb.setPatternFill(painter, Color.black);
-            pcb.rectangle(100, 400, 400, 300);
-            pcb.fillStroke();
-            pcb.sanityCheck();
-
-            document.newPage();
-
-            PdfPatternPainter painter2= PatternCanvas.diamondGrid(pcb);
-            pcb.setPatternFill(painter2, Color.black);
-            pcb.rectangle(100, 400, 400, 300);
+//            PdfPatternPainter painter= PatternCanvas.grid(pcb);
+//            pcb.setPatternFill(painter, Color.black);
+//            pcb.rectangle(100, 400, 400, 300);
+//            pcb.fillStroke();
+//            pcb.sanityCheck();
+//
+//            document.newPage();
+//            pcb.setRGBColorFill(183,183,183);
+            pcb.setRGBColorFill(255,255,255);
+//            pcb.rectangle(100, 400, 400, 50);
+//            pcb.rectangle(100, 400,378.70865F, 38.267715F );
+            pcb.rectangle(100, 400,28.34645652770996F, 28.34645652770996F );
+//            pcb.setLineWidth(1f);
+//            pcb.setColorStroke(Color.black);
+//            pcb.stroke();
+            pcb.fill();
+//            PdfPatternPainter painter2= PatternCanvas.dottedLine_5(pcb);
+            PdfPatternPainter painter2= PatternCanvas.dottedLine_40(pcb);
+//            pcb.setPatternFill(painter2, Color.BLACK);
+            pcb.setPatternFill(painter2, new Color(89,89,89));
+//            pcb.setPatternFill(painter2);
+//            pcb.rectangle(100, 400, 400, 300);
+            pcb.rectangle(100, 400,28.34645652770996F, 28.34645652770996F );
             pcb.fill();
             pcb.sanityCheck();
-
-
             document.close();
             pdfWriter.close();
             System.out.println("Object drawn on pdf successfully");
